05ff044689eecf50e1d62f807c9222cb7d0fc32a
xc-racer99 <xc-racer2@live.ca>
2017-09-10 20:32:09 -0700

    Add an icon

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@ int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
 
+    app.setWindowIcon(QIcon(":/images/images/icon.png"));
+
     MainWindow window;
 
     window.showMaximized();
